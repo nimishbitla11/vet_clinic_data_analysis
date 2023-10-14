448e71eb7e13398701efb8c515c71f357b344dda
nimishbitla11 <nimishbitla11@gmail.com>
2023-10-14 23:46:34 +0530

    creating proceduresdetails table

---
+++ proceduresdetails.sql
@@ -0,0 +1,10 @@
+-- creating table called proceduresdetails 
+
+CREATE TABLE proceduredetails (
+    proceduretype varchar,
+    proceduresubcode varchar,
+    description varchar,
+    price float
+);
+
+
