17ac03a35da4acf14058634f646c1284b609bda9
nimishbitla11 <nimishbitla11@gmail.com>
2023-10-14 23:49:00 +0530

    creating procedures history table

---
+++ procedurehistory.sql
@@ -0,0 +1,9 @@
+-- creating table procedures history table
+
+CREATE TABLE procedurehistory (
+    petid varchar,
+    proceduredate date,
+    proceduretype varchar,
+    proceduresubcode varchar
+);
+
