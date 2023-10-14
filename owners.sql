d3fcbd50aef79e52c79158bd340acae8cc387de4
nimishbitla11 <nimishbitla11@gmail.com>
2023-10-14 23:44:35 +0530

    creating owners table

---
+++ owners.sql
@@ -0,0 +1,14 @@
+-- create owners table using SQL in postgreSQL
+
+CREATE TABLE owners (
+    ownerid varchar,
+    name varchar,
+    surname varchar,
+    streetaddress varchar,
+    city varchar,
+    state varchar(2),
+    statefull varchar,
+    zipcode varchar
+);
+
+
