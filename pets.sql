c4812e1a976f3e8243a155c7a2237b3648d35ea9
nimishbitla11 <nimishbitla11@gmail.com>
2023-10-14 23:38:30 +0530

    Adding pets.sql file to create pets table

---
+++ pets.sql
@@ -0,0 +1,11 @@
+-- Command's to create table called pets in PostgreSQL 
+
+CREATE TABLE pets (
+    petid varchar,
+    name varchar,
+    kind varchar,
+    gender varchar,
+    age int,
+    ownerid varchar
+);
+
